699493b85d4605504b7e772666cd77faa5560bac
sypatrick <booth20037@gmail.com>
2022-09-20 01:38:35 +0900

    validation

---
+++ ValidationAbsrtraction/src/main/java/com/example/validationabsrtraction/ValidationAbsrtractionApplication.java
@@ -0,0 +1,13 @@
+package com.example.validationabsrtraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationAbsrtractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationAbsrtractionApplication.class, args);
+	}
+
+}
---
+++ ValidationAbsrtraction/src/test/java/com/example/validationabsrtraction/ValidationAbsrtractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validationabsrtraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationAbsrtractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
