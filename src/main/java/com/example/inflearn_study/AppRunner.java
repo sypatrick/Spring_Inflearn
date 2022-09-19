d5ad8c06cd3e0890187e0c22bdceacfa912804c4
patrick <booth20037@gmail.com>
2022-09-19 21:12:39 +0900

    Resource 추상화

--- src/main/java/com/example/inflearn_study/AppRunner.java
+++ src/main/java/com/example/inflearn_study/AppRunner.java
@@ -1,9 +1,11 @@
 package com.example.inflearn_study;
 
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -21,7 +23,8 @@ public class AppRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        System.out.println(resourceLoader.getClass());
+//        var ctx = new ClassPathXmlApplicationContext("app.xml");
+
         Resource resource = resourceLoader.getResource("classpath:test.txt");
 
 
@@ -30,7 +33,7 @@ public class AppRunner implements ApplicationRunner {
 
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
-        System.out.println(Files.lines(path));  // java11 버전에선 readString메소드 사용
+        System.out.println(Files.readAllLines(path));  // java11 버전에선 readString메소드 사용
 
     }
 }
