ac964e78645f8701f1813022b1a5bbbfcd463433
sypatrick <booth20037@gmail.com>
2022-09-19 22:59:49 +0900

    ValidatationAbstraction

---
+++ demo1/ValidationAbstraction/src/test/java/com/example/validationabstraction/ValidationAbstractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validationabstraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationAbstractionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
