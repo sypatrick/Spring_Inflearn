c73e7b18fdd94bdbe0a9c19ccb05bb92bb183cc6
sypatrick <booth20037@gmail.com>
2022-09-20 22:16:15 +0900

    Validation_0920

--- ValidationAbsrtraction/src/main/java/com/example/validationabsrtraction/AppRunner.java
+++ ValidationAbsrtraction/src/main/java/com/example/validationabsrtraction/AppRunner.java
@@ -1,24 +1,33 @@
 package com.example.validationabsrtraction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
+    @Autowired
+    Validator validator;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(validator.getClass());
+
         Event event = new Event();
-        EventValidator eventValidator = new EventValidator();
+
+        event.setEmail("asdfasdf");
+        event.setLimit(-1);
+
         Errors errors = new BeanPropertyBindingResult(event, "event");
 
-        eventValidator.validate(event, errors);
+        validator.validate(event, errors);
 
         System.out.println(errors.hasErrors());
 
--- ValidationAbsrtraction/src/main/java/com/example/validationabsrtraction/Event.java
+++ ValidationAbsrtraction/src/main/java/com/example/validationabsrtraction/Event.java
@@ -1,10 +1,23 @@
 package com.example.validationabsrtraction;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
 public class Event {
 
     Integer id;
 
+    @NotEmpty
     String title;
+
+    @Email
+    String email;
+
+    @NotNull @Min(0)
+    Integer limit;
 
     public Integer getId() {
         return id;
@@ -20,5 +33,21 @@ public class Event {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
     }
 }
