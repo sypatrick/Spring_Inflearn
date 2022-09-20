c873bbaef72de000de34d39cd6aea909dcf57dea
sypatrick <booth20037@gmail.com>
2022-09-20 21:29:20 +0900

    Validation_0920

--- ValidationAbsrtraction/src/main/java/com/example/validationabsrtraction/EventValidator.java
+++ ValidationAbsrtraction/src/main/java/com/example/validationabsrtraction/EventValidator.java
@@ -8,12 +8,12 @@ public class EventValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+        return Event.class.equals(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "notempty","Empty title is not allowed");
-        System.out.println("후..");
+
     }
 }
